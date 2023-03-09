8b79880e526ff21becd4ede19cc5b020620643b1
Matilda0841 <jr0841@naver.com>
2023-03-10 00:10:24 +0900

    정올 예제 출력문
    JAVA Day 4 출력 파트 정올 연습 문제 올린 페이지
    
    https://cording0841.tistory.com/41

---
+++ src/Println/Println_Jungol.java
@@ -0,0 +1,116 @@
+package src.Println;
+
+public class Println_Jungol {
+    public static void main(String[] args) {
+
+//        501
+//        Fun Programming!
+        System.out.println("Fun Programming!");
+        System.out.println();
+
+//        502
+//        Programming! It's fun.
+        System.out.println("Programming! It's fun.");
+        System.out.println();
+
+//        503
+//        My name is Hong Gil Dong.
+        System.out.println("My name is Hong Gil Dong.");
+        System.out.println();
+
+//        504
+//        (@) (@)
+//        (=^.^=)
+//        (-m-m-)
+        System.out.println("(@) (@)");
+        System.out.println("(=^.^=)");
+        System.out.println("(-m-m-)");
+        System.out.println();
+
+//        505
+//        I can program well.
+//        Dreams come ture.
+        System.out.println("I can program well.");
+        System.out.println("Dreams come true.");
+        System.out.println();
+
+//        506
+//        My height
+//        170
+//        My weight
+//        68.600000
+        System.out.println("My height");
+        System.out.println("170");
+        System.out.println("My weight");
+        System.out.println("68.600000");
+        System.out.println();
+
+//        507
+//        5 Dan
+//        5 * 2 = 10
+        System.out.println("5 Dan");
+        System.out.println("5 * 2 = " +5*2);
+        System.out.println();
+
+//        508
+//        item count price
+//        pen 20 100
+//        note 5 95
+//        eraser 110 97
+        System.out.printf("%10s%10s%10s\n","item","count","price");
+        System.out.printf("%10s%10d%10d\n","pen",20,100);
+        System.out.printf("%10s%10d%10d\n","note",5,95);
+        System.out.printf("%10s%10d%10d\n","eraser",110,97);
+        System.out.println();
+
+//        101
+//        My name is hong
+        System.out.println("My name is Hong");
+        System.out.println();
+
+//        102
+//        My hometown
+//        Flowering mountain
+        System.out.println("My hometown");
+        System.out.println("Flowering mountain");
+        System.out.println();
+
+//        103
+//        TTTTTTTTTT
+//        TTTTTTTTTT
+//            TT
+//            TT
+//            TT
+        System.out.println("TTTTTTTTTT");
+        System.out.println("TTTTTTTTTT");
+        System.out.println("    TT");
+        System.out.println("    TT");
+        System.out.println("    TT");
+        System.out.println();
+
+//        104
+//        kor 90
+//        mat 80
+//        eng 100
+//        sum 270
+//        avg 90
+        System.out.println("kor 90");
+        System.out.println("mat 80");
+        System.out.println("eng 100");
+        System.out.println("sum "+ (90+80+100));
+        System.out.println("avg "+ ((90+80+100)/3));
+        System.out.println();
+
+//        105
+//        Seoul 10,312,545 +91,375
+//        Pusan 3,567,910 +5,868
+//        Incheon 2,758,296 +64,888
+//        Daegu 2,511,676 +17,230
+//        Gwangju 1,454,636 +29,774
+        System.out.printf("%15s%15s%15s\n","Seoul","10,312,545","+91,375");
+        System.out.printf("%15s%15s%15s\n","Pusan","3,567,910","+5,868");
+        System.out.printf("%15s%15s%15s\n","Incheon","2,758,296","+64,888");
+        System.out.printf("%15s%15s%15s\n","Daegu","2,511,676","+17,230");
+        System.out.printf("%15s%15s%15s\n","Gwangju","1,454,636","+29,774");
+    }// 메서드
+} //클래스
